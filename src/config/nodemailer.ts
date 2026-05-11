fcc0af7a83b04aba855b146555790a26509b454d
Elias Chan <chanelias015@gmail.com>
2026-05-12 07:35:10 +0800

    fixed signup for users.

--- src/config/nodemailer.ts
+++ src/config/nodemailer.ts
@@ -3,11 +3,11 @@ import SMTPTransport from "nodemailer/lib/smtp-transport";
 
 // Create Nodemailer transporter for Brevo
 export const transporter = nodemailer.createTransport({
-  host: process.env.SMTP_HOST!,
-  port: Number(process.env.SMTP_PORT!),
-  secure: Number(process.env.SMTP_PORT) === 465, // true for 465, false for others
-  auth: {
-    user: process.env.SMTP_USER!,
-    pass: process.env.SMTP_PASS!,
-  },
+    host: process.env.SMTP_HOST!,
+    port: 465,
+    secure: true,
+    auth: {
+        user: process.env.SMTP_USER!,
+        pass: process.env.SMTP_PASS!,
+    },
 } as SMTPTransport.Options);
